466331ea65543b7955b8d5029a0f57d36738a34d
Milos Spasovski <spasovski_milos@hotmail.com>
2021-03-20 14:47:04 +0000

    Gitpod

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ function Footer()
 {
     return (
         <footer className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
-            &copy; Copyright 2020
+            &copy; Copyright 2021
         </footer>
     )
 
